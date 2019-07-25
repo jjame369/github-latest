d7f344451d9aa1ba516c23ab098663d7fc690867
Justin Jameson <42359064+jjame369@users.noreply.github.com>
2019-07-24 17:41:26 -0700

    completed activity

--- main.py
+++ main.py
@@ -1,20 +1,19 @@
 import sys
 import json
-
 import requests
 
-# Use Like python githubber.py JASchilz
-# (or another user name)
-
 if __name__ == "__main__":
+    # Receive a GitHub username from the command line
     username = sys.argv[1]
+    #username = 'jjame369'
 
-    # TODO:
-    #
-    # 1. Retrieve a list of "events" associated with the given user name
-    # 2. Print out the time stamp associated with the first event in that list.
+    response = requests.get("https://api.github.com/users/{}/events".format(username))
+    # Retrieve a list of "events" associated with that user. User events include things like pushing to a repository
+    # or opening up an issue on a repository.
+    events = json.loads(response.content)
 
-    print("COMPLETE THE TODOs")
-    
+    # Print out the time stamp associated with the first event in that list
+    print(events[0]['created_at'])
+
 
 
